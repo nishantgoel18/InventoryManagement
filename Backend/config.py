a5194730035bbcbab757f3cbde3b401f9abf6e68
Nishant Goyal <nishant@pluralcode.com>
2026-06-19 14:07:25 +0530

    deployment fixes

--- Backend/config.py
+++ Backend/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://@localhost:5432/inventory_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inventory_db_n2kr_user:pzzfLziEr28CFRiEVDN41EHhlearDMt6@dpg-d8qfuim8bjmc738mihk0-a/inventory_db_n2kr')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your-secret-key-change-in-prod')
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'uploads/')
